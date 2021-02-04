4f6c74595796f93fcd836c253c8ec3dfde5274ea
TwoStep <71077143+DanielStepic@users.noreply.github.com>
2021-02-04 13:16:31 +0100

    Add files via upload

---
+++ pygames/ReadMe.txt
@@ -0,0 +1,92 @@
+README 
+
+BESCHREIBUNG:
+Das Spiel soll eine Version zeigen, was nach dem Spiel AmongUs mit den Charakteren passiert die unschuldig verstoßen wurden.
+Hierbei landet ein Grüner Among auf den Planeten "ZeroTwo", der kleine Among wurde wie viele anderen unschuldig verstoßen.
+Auf den Planeten gelandet, bemerkt der Among das er nicht alleine auf diesen Planeten ist. Anscheinend  haben sich Blauewesen schon ausgebreitet.
+Schnell bemerkt der Among das diese Wesen nicht freundlich gesinnt sind.Der Grüne Among hat auch leider keine Waffen die er benutzen kann um sich zu verteidigen.
+Nach dem der Among etwas mehr ausschau gehalten hat, bemerkt er das in weiter Ferne ein Flagge zu sehen ist. Was hat dies zu beudeten? Gibt es eventuell mehr überlebende ?
+Um dies Herauszufinden begibt sich der Among auf die Mysteriöse Reise! Beachte du musst den Gegner aus den weg gehen bzw umgehen!
+
+
+
+WARUM HABE ICH DAS SPIEL PROGRAMMIERT?
+Da ich das Spiel AmongUs sehr bewundere wollte ich eine kleine Version über die Verstoßung der Characktere machen die nicht der "Imposter" waren, machen bzw entwickeln.
+Dies soll veranschaulichen, was wäre wenn... :D
+
+
+
+WIE IST DAS SPIEL AUFGEBAUT?
+Das Spiel ist so aufgebaut das der Spieler einen verstoßenen Grünen Amongcharakter spielt, der zufällig auf einen Mysteriösen Planeten gelandet ist.
+Doch der Charakter ist nicht alleine, einige Blauwesen(Gegner) sind Bewohner dieses Planeten und sind feindlich gesinnt.
+Eine Weise Flagge soll darauf hindeuten das es evtl weitere Amongs auf dem Planten gibt.
+Das Ziel ist es also bis zur Weißen Falgge zu kommen ohne zu sterben.
+Da der Characketer noch keine Waffen besitzt muss der Spieler mit geschick sich an den Gegner vorbei schleichen.
+Das Spiel hat zuzeit nur ein Level.
+Es werden weitere Level später hinzugefügt, wo man dann auch eine Waffe bekommt!
+
+
+STEUERUNG:
+Das ganze Spiel wird mit den Pfeil-Tasten betrieben! 
+Pfeil-Taste-Rechts: Laufen nach Rechts!  -->
+Pfeil-Taste-Links: Laufen nach Links!    <--
+Pfeil-Taste-Oben: Springen! 		  ^
+(Mehr Tasten werden nicht benötigt)
+
+
+
+ZU BEACHTEN:
+Beachte es gibt einen kleinen Timer beim springen, das führt dazu das man erst nach gewisser zeit nach einem Sprung, einen weiteren Sprung durchführen kann!!!
+Beachte das im ersten Level keine Waffen vorhanden sind!!
+
+
+CONFIG-DATEI:
+Eine Config ist nicht direkt enthalten!
+Es gibt eine txt-Datei die "MAP" heist, dort kann man beliebig die Map gestallten wobei 0 = ist nichts 1 = der Boden und 2= der Flur!
+Dadurch kann man sich auch persöhnliche Levels erschaffen oder bauen!
+
+
+KLASSEN:
+Das Spiel besitzt zurzeit nur eine Klasse und zwar die Gegner-Klasse! Wozu das ?
+Die Gegner werden immer die selben sein, da sie auch die selben Werte haben sollen ist eine Klasse sehr nützlich!
+Weiterhin kann man dadurch weitere Gegner ziemlich einfach in das Spiel einbauen.
+Später wird dies auch in eine CONFIG-DATEI geschrieben das der Spieler auch beliebige Gegner setzten kann!
+Eine Spieler Klasse gibt es noch nicht da, dies noch zu Problemen der Collision führt. Wir bitten um Verständis :)
+Später soll die Spieler-Klasse für die States stehen wodurch der Spieler sich auch etwas pushen kann.
+
+
+PYTHON-PAKETE:
+Pythoon-Pakete wurden nur für die Musik einbindung benötigt.Warum?
+Jedes Spiel braucht bestimmte Töne das es auch ein wiedererkennungsmerkmal besitzt.
+Das Menu wurde auch aus einer Bibliothek importiert
+"pygame-Menu"
+
+MUSIK:
+Die Themen-Musik wurde von mir persöhnlich auf der Gitarre gespielt!
+Die Sound-Effekte wurden aus bestimmten Bibliotheken entnommen!
+
+
+CHARCKTER:
+Charakter soll eine Anspielung auf das Spiel AmongUs sein!
+Grünes Among gibt es in diesem Fall auch in dem Spiel "AmongUs"!
+
+
+GEGNER:
+Die Inpiration der Gegner habe ich auf der Seite:www.kenny.nl
+Wieso blau? 
+Blau ist meine Lieblingsfarbe :)
+
+
+GRAFIKEN:
+Gegner = www.kenny.nl
+Charckter = grüner Among in Google eintippen
+Flagge = www.google.de weiße Spiel- Flagge
+Bausteine = www.kenny.nl
+Hintergrund = google --> weltraum hintergründe
+
+
+
+
+
+
+
---
+++ pygames/ZeroTwo.py
@@ -0,0 +1,380 @@
+import pygame, sys# import pygame and sys
+from os import path #aktiviert Pfad 
+from game import Game
+
+
+clock = pygame.time.Clock() # set up the clock
+
+from pygame.locals import * # import pygame modules
+pygame.init() # initiate pygame
+pygame.mixer.init()
+
+#MUSIK wird INITIALISIERT-----------------------------------------------------
+music = path.join(path.dirname(__file__), 'Music')#Musik einschalten
+jumpsound = pygame.mixer.Sound(path.join(music,'Jump.wav'))# Sprung-Sound hinzugefügt
+
+
+
+#BILDSCHIRM GRÖßEN ------------------------------------------------------------------------------------
+pygame.display.set_caption('ZeroTwo') # set the window name
+
+SCREEN_SIZE = 800
+WINDOW_SIZE = (SCREEN_SIZE,600) # set up window size
+
+screen = pygame.display.set_mode(WINDOW_SIZE,0,32) # initiate screen
+
+display = pygame.Surface((300, 200))
+bg = pygame.image.load('Grafiken/bg2.png') #Hintergrund-Bild
+bg_rect= bg.get_rect()                     # Hintergrund_rect
+sieg= pygame.image.load('Grafiken/gewonnen2.jpg')
+lose = pygame.image.load('Grafiken/verloren2.png')
+flage =pygame.image.load('Grafiken/flage-0.png')
+
+#IMAGES--------------------------------------------------------------------------------------------------
+player_image = pygame.image.load('Grafiken/tenor-0.png').convert()
+player_image.set_alpha(250)
+player_image.set_colorkey((0,0,0))
+player_death = pygame.image.load('Grafiken/tot.png')
+
+
+laufen_rechts = [pygame.image.load('Grafiken/tenor-0.png'), pygame.image.load('Grafiken/tenor-1.png'),pygame.image.load('Grafiken/tenor-2.png'),pygame.image.load('Grafiken/tenor-3.png'),pygame.image.load('Grafiken/tenor-4.png')]
+laufen_links = [pygame.image.load('Grafiken/tenor-5.png'), pygame.image.load('Grafiken/tenor-6.png'),pygame.image.load('Grafiken/tenor-7.png'),pygame.image.load('Grafiken/tenor-8.png'),pygame.image.load('Grafiken/tenor-9.png')]
+lauf_zaehler = 0
+black = [0,0,0]
+
+#GEGNER-------------------------------------------------------------------------------------------------
+
+class Gegner():  #Gegner-Klasse , Klasse weil mehrere Gegner benötigt werden
+    def __init__(self, x, y, geschw, breite,height,richtg,xMin,xMax):
+        self.x = x
+        self.y = y
+        self.geschw = geschw
+        self.breite = breite
+        self.height = height
+        self.richtg= richtg
+        self.laufen_rechts= 0
+        self.laufen_links= 0
+        self.xMin = xMin
+        self.xMax= xMax
+        self.gegner_rechts = [pygame.image.load('Grafiken/gegner-00.png'), pygame.image.load('Grafiken/gegner-01.png'),pygame.image.load('Grafiken/gegner-02.png'),pygame.image.load('Grafiken/gegner-03.png'),pygame.image.load('Grafiken/gegner-04.png'),pygame.image.load('Grafiken/gegner-05.png')]
+        self.gegner_links =  [pygame.image.load('Grafiken/gegner-06.png'), pygame.image.load('Grafiken/gegner-07.png'),pygame.image.load('Grafiken/gegner-08.png'),pygame.image.load('Grafiken/gegner-09.png'),pygame.image.load('Grafiken/gegner-10.png'),pygame.image.load('Grafiken/gegner-11.png')]
+        
+    def draw_gegner(self):   #Funktion um Gegner zu zeichnen
+        if self.laufen_rechts == 10:
+            self.laufen_rechts = 0
+        if self.laufen_links == 10:
+            self.laufen_links  = 0
+            
+            
+        if self.richtg[0]:
+            screen.blit(self.gegner_links[self.laufen_links//8], (self.x,self.y))
+        if self.richtg[1]:
+            screen.blit(self.gegner_rechts[self.laufen_rechts//8], (self.x,self.y))
+            
+    
+    def Laufen(self):
+         self.x += self.geschw
+         if self.geschw > 0:
+            self.richtg = [0,1]
+            self.laufen_rechts += 1
+         if self.geschw < 0:
+            self.richtg = [1,0]
+            self.laufen_links += 1
+            
+    def hinHer(self):
+        if self.x > self.xMax:
+            self.geschw *= -1
+        elif self.x < self.xMin:
+            self.geschw *= -1
+        self.Laufen()
+        
+        
+        
+    
+#Animation------------------------------------------------------------------------------------------------
+def Gegner_zeichen():
+    
+    
+    gegner1.draw_gegner()
+    gegner1.hinHer()
+    
+    gegner2.draw_gegner()
+    gegner2.hinHer()
+   
+    if gewonnen:
+        screen.fill(black)
+        screen.blit(sieg,(0,0)) 
+        
+    elif verloren:
+        screen.fill(black)
+        screen.blit(lose,(0,10))
+       
+     
+        
+        
+    pygame.display.update()
+    
+
+def updatePlayerImage(direction):
+    global lauf_zaehler, laufen_links, laufen_rechts
+
+    if lauf_zaehler >= 32:
+        lauf_zaehler = 0
+
+    if direction == 'left':
+        player_image = laufen_links[lauf_zaehler//10]#pygame.image.load(laufen_links[lauf_zaehler//10]).convert()
+        player_image.set_alpha(250)
+        player_image.set_colorkey((0, 0, 0))
+        lauf_zaehler += 1
+    elif direction == 'right':
+        player_image = laufen_rechts[lauf_zaehler//10]#pygame.image.load(laufen_rechts[lauf_zaehler//10]).convert()
+        player_image.set_alpha(250)
+        player_image.set_colorkey((0, 0, 0))
+        lauf_zaehler += 1
+    else:
+        lauf_zaehler = 0
+        player_image = pygame.image.load('Grafiken/tenor-0.png')
+
+
+    return player_image
+
+
+player_image = updatePlayerImage(None)
+
+
+#----------MAP------------------------------------------------------------------------------------------------------
+flur_image = pygame.image.load('Grafiken/flur.png')
+tile_size = flur_image.get_width()
+
+boden_image = pygame.image.load('Grafiken/erde.png')
+
+
+#Liest die map aus txt-Datei aus
+def load_map(path):
+    f = open(path + '.txt','r')
+    data = f.read()
+    f.close()
+    data = data.split('\n')
+    game_map = []
+    for row in data:
+        game_map.append(list(row))
+    return game_map
+            
+game_map = load_map('map')
+# Die Map wird hier vorrausgesetzt 1 = Boden 2 = Flur auf dem man Läuft
+
+
+
+
+#COLLISON------------------------------------------------------------------------------------------------
+def collision_test(rect, tiles): # Kollisions Liste wegen den Rects die erschaffenw werden
+    hit_list = []
+    for tile in tiles:
+        if rect.colliderect(tile):
+            hit_list.append(tile)
+    return hit_list
+
+
+#BEWEGEN----------------------------------------------------------------------------------------------
+def move(rect, movement, tiles):   # Bewegung wenn collision Rect trifft (Was soll passieren)
+    collision_types = {'top': False, 'bottom': False, 'right': False, 'left': False}
+    rect.x += movement[0]
+    hit_list = collision_test(rect, tiles)
+    for tile in hit_list:
+        if movement[0] > 0:
+            rect.right = tile.left
+            collision_types['right'] = True
+        elif movement[0] < 0:
+            rect.left = tile.right
+            collision_types['left'] = True
+    rect.y += movement[1]
+    hit_list = collision_test(rect, tiles)
+    for tile in hit_list:
+        if movement[1] > 0:
+            rect.bottom = tile.top
+            collision_types['bottom'] = True
+        elif movement[1] < 0:
+            rect.top = tile.bottom
+            collision_types['top'] = True
+    return rect, collision_types
+
+
+
+#
+def Kollision_gegner():
+    global verloren, gewonnen, start
+    gegner1Rect= pygame.Rect(gegner1.x - player_rect.x -100, 458 - gegner1.y,gegner1.breite,gegner1.height)  #player_rect von gegnerrect abziehen und minus extra wert damit die pixel abgestimtm sind
+    gegner2Rect= pygame.Rect(gegner2.x - player_rect.x - 116 , 170 - gegner2.y, gegner2.breite, gegner2.height) # player_rect von gegnerrect abziehen und minus extra wert damit die pixel abgestimtm sind
+    
+    #print(gegner2Rect,player_rect) für Debugg
+    
+    if gegner1Rect.colliderect(player_rect):
+        verloren= True
+        start = False
+        
+    if gegner2Rect.colliderect(player_rect):
+        verloren= True
+        start = False
+    if player_rect.colliderect(flageRect):
+        gewonnen = True
+        verloren = False
+        start = False
+     
+
+
+
+moving_right = False
+moving_left = False
+
+player_y_momentum = 0
+air_timer = 0
+
+#Spieler_rect und Gegner werden erzeugt
+player_rect = pygame.Rect(50, 50, player_image.get_width(), player_image.get_height())
+gegner1= Gegner(400,355,4,13,17,[0,0],300,600) #x, y, geschw, breite,hoehe,richtg,xMin,xMax  Gegner wird erzeugt
+gegner2= Gegner(400,139,2,13,17,[0,0],300,400) #x, y, geschw, breite,hoehe,richtg,xMin,xMax   Gegner wird erzeugt
+flageRect= pygame.Rect(278,-4,17,23)
+verloren = False
+gewonnen = False
+
+
+
+#MUSIK--------------------------------------------------------------------------------------------
+pygame.mixer.music.load(path.join(music,'song1.mp3'))#Aufruf der Datei
+pygame.mixer.music.set_volume(0.4) #Volumen Musik
+pygame.mixer.music.play(loops = -1)# Musik wird immer Wiederholt
+
+
+
+
+#MENUE-----------------------------------------------------------
+
+                    
+                
+g = Game()  #Klasse von import game.py
+g.running = True
+while g.running:
+   g.curr_menu.display_menu()
+   
+   for event in pygame.event.get(): # event loop
+       if event.type == pygame.QUIT: # check for window quit
+           pygame.quit() # stop pygame
+           sys.exit()
+   g.running = False # close loop
+   
+        
+    
+    
+       
+    
+#MAIN------------------------------------------------------------------------------------------------------------------------
+start= True
+while start:
+  
+    
+    display.blit(bg,bg_rect) #Hintergrund einspielem
+    display.blit(flage,flageRect)
+    
+
+   
+    #Rect-Liste für die Map
+    tile_rects = []      
+    y = 0
+    for row in game_map:
+        x = 0
+        for tile in row:
+            if tile == '1':
+                display.blit(boden_image, (x * tile_size, y * tile_size))
+            if tile == '2':
+                display.blit(flur_image, (x * tile_size, y * tile_size))
+            if tile != '0':
+                tile_rects.append(pygame.Rect(x * tile_size, y * tile_size, tile_size, tile_size))
+            x += 1
+        y += 1
+
+ 
+
+    
+    #Spieler Bewegung
+    player_movement = [0, 0]
+    if moving_right:
+        player_movement[0] += 2
+        player_image = updatePlayerImage('right')
+    elif moving_left:
+        player_movement[0] -= 2
+        player_image = updatePlayerImage('left')
+    else:
+        player_image = updatePlayerImage(None)
+    player_movement[1] += player_y_momentum
+    player_y_momentum += 0.2
+    if player_y_momentum > 3:
+        player_y_momentum = 3
+
+
+    player_rect, collisions = move(player_rect, player_movement, tile_rects)  #Rect wird intialisiert
+    
+    
+    
+    #Damit er nicht zu lang in der Luft ist wenn der Spieler springt , verursacht auch das ständige Hüpfen :)
+    if collisions['bottom']:
+        player_y_momentum = 1
+        air_timer = 0
+    else:
+        air_timer += 1
+
+    display.blit(player_image, (player_rect.x, player_rect.y))
+
+     #Für das Schließen des Games
+    for event in pygame.event.get(): # event loop
+        if event.type == pygame.QUIT: # check for window quit
+            pygame.quit() # stop pygame
+            sys.exit() # stop script
+   
+            
+            
+            
+#-KEYS----------------------------------------------------------------------------       
+        
+            #Key zuweisungen , Tasten
+        if event.type == KEYDOWN:
+            if event.key == K_RIGHT: 
+                moving_right = True
+            if event.key == K_LEFT:
+                moving_left = True
+            if event.key == K_UP:      
+                if air_timer < 6:
+                    player_y_momentum = -5
+                    jumpsound.play()
+        if event.type == KEYUP:
+            if event.key == K_RIGHT:
+                moving_right = False
+            if event.key == K_LEFT:
+                moving_left = False
+            
+                
+              
+    
+# BILDSCHIRM AUSGABE-------------------------------------------------------------------
+   
+        
+    surf = pygame.transform.scale(display, WINDOW_SIZE)
+    screen.blit(surf, (0, 0))
+    
+    Kollision_gegner()
+    Gegner_zeichen() # Ruft die funktion auf um die Gegner zu zeichnen, keine lust jedes mal die einzlne Funktion aufzurufen
+
+    
+        
+   
+    pygame.display.update() # update display
+    pygame.display.flip()
+    clock.tick(60) # maintain 60 fps
+    
+wiederholen = True
+while wiederholen:
+            
+     for event in pygame.event.get(): # event loop
+        if event.type == pygame.QUIT: # check for window quit
+            pygame.quit() # stop pygame
+            sys.exit()
+       
